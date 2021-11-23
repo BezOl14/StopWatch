70b4818c2e7e97d2d985df17c472d8800a3ec826
Oleksii Kucherenko <91497964+Kucherenko-Oleksii@users.noreply.github.com>
2021-11-23 18:52:19 +0200

    Buttons file

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function ButtonComponent({ start, stop, reset, wait, status }) {
+  return (
+      <div>
+          {(status === 0) ?
+            <div>
+                <button onClick={start}>Start</button>
+                <button onClick={wait}>Wait</button>
+            </div> : ""                
+          }
+
+          {(status === 1) ?
+              <div>
+                  <button onClick={stop}>Stop</button>
+                  <button onClick={reset}>Reset</button>                
+              </div> : ""
+          }
+
+          {(status === 2) ?
+              <div>         
+                  <button onClick={reset}>Reset</button>                    
+              </div> : ""
+          }
+      </div>
+  );
+}
+
+export default ButtonComponent;
